aeffbddd3ab917ba9a894ceb2aa6f5f2ddb727e1
MartinNav <martinnavratilu@gmail.com>
2024-11-11 21:05:11 +0100

    added generate random str

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::char;
+
+use rand::{rngs::ThreadRng, RngCore};
+
+fn generate_random_str_of_rn_len(rng:&mut ThreadRng)->String{
+    const MAX_LEN:usize=6000;
+    let len:usize = (rng.next_u32()as usize)%MAX_LEN;
+    let mut res = String::with_capacity(len);
+    for _ in 0..len {
+        res.push(char::from_u32(rng.next_u32()).unwrap_or('.'));
+    }
+    res
+}
 fn main() {
-    println!("Hello, world!");
+    let mut num_of_cysles=6669;
+    let mut rng = rand::thread_rng();
+    loop {
+        let random_chars =generate_random_str_of_rn_len(&mut rng);
+        
+        if num_of_cysles==0 {
+            break;
+        }
+        num_of_cysles-=1;
+    }
 }
